76b707f05a9cca995e011981ef06c7a6a77e10d8
ogwurujohnson <ogwurujohnson@gmail.com>
2020-05-03 16:39:15 +0100

    demonstrate hashing a block

---
+++ blocks/block.js
@@ -0,0 +1,24 @@
+const {SHA256} = require('crypto-js');
+
+class Block {
+    constructor(data){
+		this.id = 0;
+        this.nonce = 144444;
+        this.body = data;
+        this.hash = "";
+    }
+
+    async generateHash() {
+        let self = this;
+        try {
+            const stringifyObj = JSON.stringify(self.body);
+            self.hash = SHA256(stringifyObj);
+            return self.hash;
+        } catch(err) {
+            console.log(err)
+        }
+        
+    }
+}
+
+module.exports.Block = Block;
